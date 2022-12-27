9904f4edb4707f37ff29a41245a83ea95a49b47a
Tony V <vaninanton@gmail.com>
2022-12-27 15:15:25 +0600

    Init Type model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(BrandSeeder::class);
+        $this->call(TypeSeeder::class);
     }
 }
