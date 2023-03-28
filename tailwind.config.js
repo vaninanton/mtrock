df6db774f9467a1e4df52c991d27fa67de479965
Shift <shift@laravelshift.com>
2023-03-28 12:16:12 +0000

    Streamline Tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
@@ -8,11 +9,10 @@ module.exports = {
         './resources/views/**/*.blade.php',
         './node_modules/flowbite/**/*.js',
     ],
-
     theme: {
         extend: {
             colors: {
-                'primary': {
+                primary: {
                     light: '#5ca3fc',
                     DEFAULT: '#0350b1',
                     dark: '#033f8c',
@@ -23,10 +23,5 @@ module.exports = {
             },
         },
     },
-
-    plugins: [
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/typography'),
-        require('flowbite/plugin'),
-    ],
-};
+    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography'), require('flowbite/plugin')],
+}
