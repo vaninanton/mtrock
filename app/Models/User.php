3fe6c43780f77f5b99eee870ee5e2e6ea8eb1bb3
Tony V <vaninanton@gmail.com>
2023-04-09 19:40:02 +0600

    wip

--- app/Models/User.php
+++ app/Models/User.php
@@ -66,4 +66,9 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function canAccessFilament(): bool
+    {
+        return $this->hasVerifiedEmail();
+    }
 }
