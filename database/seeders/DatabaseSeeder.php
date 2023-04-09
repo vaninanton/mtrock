daa095d3eadbc264fcf301bcb77497b2fc4ade76
Tony V <vaninanton@gmail.com>
2023-04-09 19:36:17 +0600

    wip

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(CallbackSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(BrandSeeder::class);
