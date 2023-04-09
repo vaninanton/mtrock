1cf7e355e6a0d9a0a46cb9eed5d199b119ead9f1
Tony V <vaninanton@gmail.com>
2023-04-09 19:59:13 +0600

    wip

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -21,5 +21,10 @@ class UserSeeder extends Seeder
             'email' => 'vaninanton@gmail.com',
             'password' => '$2y$10$JHWdWDKkXH3YxKA4Nnd7DuQAGf6z4S04W2V.C25EW0ESPO5eR5e36',
         ]);
+        User::factory()->create([
+            'name' => 'Сережа Волков',
+            'email' => 'osaaka@yandex.ru',
+            'password' => '$2y$10$wn6VjZdeNsGoARn8u8ULBeyPThu4I2QhnGpHINSdqG.hHz3Oiw3oS',
+        ]);
     }
 }
