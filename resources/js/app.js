a119e9d0a76abd5857a808a8899cef1fac8cfd4a
Tony V <vaninanton@gmail.com>
2023-04-17 00:00:26 +0600

    vue installed

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,15 @@
-import 'flowbite';
+import "flowbite";
 
-let jsFilterForm = document.querySelector('#js-filter-form');
+let jsFilterForm = document.querySelector("#js-filter-form");
 if (jsFilterForm) {
-    jsFilterForm.addEventListener('submit', function (e) {
+    jsFilterForm.addEventListener("submit", function (e) {
         e.preventDefault();
         let formData = new FormData(e.target);
         let data = [...formData.entries()];
 
         let query = [];
-        data.forEach(element => {
-            if (element[1] !== '') {
+        data.forEach((element) => {
+            if (element[1] !== "") {
                 query.push(element);
             }
         });
